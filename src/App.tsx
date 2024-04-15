c03d055b2a55600ee4fbe9bf1972ba4c755f352b
cotrones <tomcotroneo@gmail.com>
2024-04-15 03:04:21 -0400

    ts eslint postcss tailwind config and coming soon page init

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#84a98c] to-[#283f2c]">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            Pocket Watcher
+          </h1>
+          <p className="text-2xl text-white">Coming soon!!</p>
+        </div>
+      </main>
     </>
   );
 }
